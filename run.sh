3fdde51c53092b556ca8b9067a907daa5f9a600a
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2023-10-14 12:49:47 -0400

    add DETR paper-review

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 make build
-git add :/
-echo "💬 Enter Commit Message: ⤵️"
+echo "==============================="
+git status
+echo "==============================="
+echo "🤖 What do you want to track using 'git add ...' ⤵️"
+read -r files
+git add "$files"
+echo "==============================="
+echo "🤖 Enter Commit Message: ⤵️"
 read -r message
 git commit -m "$message"
+echo "==============================="
 git push
